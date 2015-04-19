3121c70806a257725f672395afffd998d91095b0
Mikael Wallin <mikael.wallin@gmail.com>
2015-04-19 18:59:28 +0200

    Solution to exercise 4.1

--- 4.1 The Sum of a Range/app.js
+++ 4.1 The Sum of a Range/app.js
@@ -24,20 +24,55 @@
 */
 
 
-console.log(CountChar("kakkerlak", "k"));
+Sum_Array(FillNumContainer(1, 10, 1));
+//console.log(Sum_Array(FillNumContainer(1, 10, 1)));
 
 
-function CountChar(IncomingText, LookFor) {
+function FillNumContainer(NumBegin, NumEnd, NumStep) {
 
-  var CharOccurences = 0;
+  var NumContainer = [];
 
-  for (var Runs = 0; Runs <= IncomingText.length; Runs++) {
 
-    if (IncomingText.charAt(Runs) === LookFor)
-      CharOccurences++;
+  if (typeof NumStep !== "number")
+    NumStep = 1;
+
+
+  if (NumStep < 0) {
+
+    for (var Runs = NumBegin; Runs >= NumEnd; Runs += NumStep)
+      NumContainer.push(Runs);
 
   }
 
-  return CharOccurences;
+  else if (NumStep > 0) {
+
+    for (var Runs = NumBegin; Runs <= NumEnd; Runs += NumStep)
+      NumContainer.push(Runs);
+
+  }
+
+
+  console.log("Array: " + NumContainer);
+
+  return NumContainer;
+
+}
+
+
+function Sum_Array(NumArray) {
+
+  var SumOfArray = 0;
+
+
+  for (var Runs = 0; Runs < NumArray.length; Runs++) {
+
+    SumOfArray += NumArray[Runs];
+
+  }
+
+
+  console.log("Sum of Array: " + SumOfArray);
+
+  return SumOfArray;
 
 }
